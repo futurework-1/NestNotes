a637e782be7da068b54e95a4854e3332c433fee8
Adam <tabievadam@gmail.com>
2025-07-03 12:29:55 +0200

    First preparing

---
+++ NestNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NestNotes
+//
+//  Created by Адам Табиев on 03.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NestNotes/NestNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestNotesApp.swift
+//  NestNotes
+//
+//  Created by Адам Табиев on 03.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct NestNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
